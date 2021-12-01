bbda75077211aa5d40e439ca879c15b2f5ba092f
Mark Wanders <mndmwanders@gmail.com>
2021-12-01 09:32:22 +0100

    Improved day 1 part 2

--- day01/Sonar.kt
+++ day01/Sonar.kt
@@ -4,5 +4,6 @@ private val input = File("day01/input.txt").readLines().map { it.toInt() }
 
 fun main() {
     println("Part 1: ${input.filterIndexed { i, s -> i > 0 && s > input[i - 1] }.size}")
-    println("Part 2: ${input.filterIndexed { i, s -> i > 0 && i < input.size - 2 && s + input[i + 1] + input[i + 2] > input[i - 1] + s + input[i + 1] }.size}")
+    // Use the fact that A + B + C > B + C + D if D > A
+    println("Part 2: ${input.filterIndexed { i, s -> i < input.size - 3 && s < input[i + 3] }.size}")
 }
