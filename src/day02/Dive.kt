13af4cfe255a45eb25d196d70308d28118568af8
Mark Wanders <mndmwanders@gmail.com>
2021-12-02 09:47:05 +0100

    Refactored day 2

--- src/day02/Dive.kt
+++ src/day02/Dive.kt
@@ -7,28 +7,18 @@ private val input = File("src/day02/input.txt").readLines()
 fun main() {
     var x = 0
     var y = 0
-    input.forEach {
-        val step = it.split(" ")
-        when (step[0]) {
-            "forward" -> x += step[1].toInt()
-            "down" -> y += step[1].toInt()
-            "up" -> y -= step[1].toInt()
-        }
-    }
-    println("Part 1: ${x * y}")
-    x = 0
-    y = 0
     var aim = 0
-    input.forEach {
-        val step = it.split(" ")
-        when (step[0]) {
-            "forward" -> {
-                x += step[1].toInt()
-                y += aim * step[1].toInt()
+    input.forEach { line ->
+        line.split(" ").let { step ->
+            when (step[0]) {
+                "forward" -> {
+                    x += step[1].toInt()
+                    y += aim * step[1].toInt()
+                }
+                "down" -> aim += step[1].toInt()
+                "up" -> aim -= step[1].toInt()
             }
-            "down" -> aim += step[1].toInt()
-            "up" -> aim -= step[1].toInt()
         }
     }
-    println("Part 2: ${x * y}")
+    println("Part 1: ${x * aim}, Part 2: ${x * y}")
 }
