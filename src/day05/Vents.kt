c998320852886ae3166eda49f2754007f45e226c
Mark Wanders <mndmwanders@gmail.com>
2021-12-05 11:13:14 +0100

    Solved day 5 part 2

--- src/day05/Vents.kt
+++ src/day05/Vents.kt
@@ -5,26 +5,32 @@ import java.io.File
 private val input = File("src/day05/input.txt").readLines()
 
 fun main() {
-    val seen = mutableMapOf<Pair<Int, Int>, Int>()
-    input.map { line -> Line(line.split(" -> ", ",").map { it.toInt() }) }
+    var seen = mutableMapOf<Pair<Int, Int>, Int>()
+    input.parse()
         .filter { line -> line.x1 == line.x2 || line.y1 == line.y2 }
-        .forEach { line ->
-            println("(${line.x1}, ${line.y1}) to (${line.x2}, ${line.y2})")
-            if (line.y1 == line.y2) {
-                for (x in line.x().minOrNull()!!..line.x().maxOrNull()!!) {
-                    seen[Pair(x, line.y1)] = seen.getOrDefault(Pair(x, line.y1), 0) + 1
-                }
-            } else {
-                for (y in line.y().minOrNull()!!..line.y().maxOrNull()!!) {
-                    seen[Pair(line.x1, y)] = seen.getOrDefault(Pair(line.x1, y), 0) + 1
-                }
-            }
-        }
-    println(seen.values.count { it > 1 })
+        .drawLines(seen)
+    println("Part 1: ${seen.values.count { it > 1 }}")
+    seen = mutableMapOf()
+    input.parse()
+        .drawLines(seen)
+    println("Part 2: ${seen.values.count { it > 1 }}")
 }
 
-class Line(val x1: Int, val y1: Int, val x2: Int, val y2: Int) {
-    constructor(coordinates: List<Int>) : this(coordinates[0], coordinates[1], coordinates[2], coordinates[3])
-    fun x() :List<Int> = listOf(x1, x2)
-    fun y() :List<Int> = listOf(y1, y2)
+fun List<String>.parse(): List<Line> = map { line ->
+    val (x1, y1, x2, y2) = line.split(" -> ", ",").map { it.toInt() }
+    Line(x1, y1, x2, y2)
 }
+
+fun List<Line>.drawLines(seen: MutableMap<Pair<Int, Int>, Int>) = forEach { line ->
+    val dx = line.x2.compareTo(line.x1)
+    val dy = line.y2.compareTo(line.y1)
+    var x = line.x1
+    var y = line.y1
+    while (x != line.x2 + dx || y != line.y2 + dy) {
+        seen[Pair(x, y)] = seen.getOrDefault(Pair(x, y), 0) + 1
+        x += dx
+        y += dy
+    }
+}
+
+class Line(val x1: Int, val y1: Int, val x2: Int, val y2: Int)
