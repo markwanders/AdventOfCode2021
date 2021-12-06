b653f1014e13d46d40becb1038f7c8d5425ad1cb
Mark Wanders <mndmwanders@gmail.com>
2021-12-06 10:32:15 +0100

    Solved day 6 part 2

--- src/day06/Anglerfish.kt
+++ src/day06/Anglerfish.kt
@@ -5,11 +5,19 @@ import java.io.File
 private val input = File("src/day06/input.txt").readText().split(",").map { it.toInt() }.toMutableList()
 
 fun main() {
-    (0..79).forEach { _ ->
-        input.forEachIndexed { index, value -> input[index] = value - 1 }
-        val newFish = input.count { it < 0 }
-        input.forEachIndexed { index, value -> input[index] = if(value < 0) 6 else value }
-        input.addAll(MutableList(newFish) { 8 })
+    println("Part 1: ${spawnFish(80)}")
+    println("Part 2: ${spawnFish(256)}")
+}
+
+fun spawnFish(days: Int): Long {
+    val fish = input.groupingBy { it }.eachCount().mapValues { it.value.toLong() }.toMutableMap()
+    (0 until days).forEach { _ ->
+        val spawned = fish.getOrDefault(0, 0)
+        (0..8).forEach {
+            fish[it] = fish.getOrDefault(it + 1, 0)
+        }
+        fish[6] = fish.getOrDefault(6, 0) + spawned
+        fish[8] = fish.getOrDefault(8, 0) + spawned
     }
-    println("Part 1: ${input.size}")
+    return fish.values.sum()
 }
