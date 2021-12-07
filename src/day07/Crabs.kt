fc6fd0bfeaadefc9c15244462d3183151a60f536
Mark Wanders <mndmwanders@gmail.com>
2021-12-07 12:27:12 +0100

    Solved day 7 part 2

--- src/day07/Crabs.kt
+++ src/day07/Crabs.kt
@@ -6,8 +6,12 @@ import kotlin.math.abs
 private val input = File("src/day07/input.txt").readText().split(",").map { it.toInt() }
 
 fun main() {
-    val part1 = input.toSet()
+    val part1 = IntRange(input.minOrNull()!!, input.maxOrNull()!!)
         .associateBy({ it }, { input.sumOf { position -> abs(position - it) } })
         .minByOrNull { it.value }!!.value
     println("Part 1: $part1")
+    val part2 = IntRange(input.minOrNull()!!, input.maxOrNull()!!)
+        .associateBy({ it }, { input.sumOf { position -> IntRange(1, abs(position - it)).sum() }})
+        .minByOrNull { it.value }!!.value
+    println("Part 2: $part2")
 }
