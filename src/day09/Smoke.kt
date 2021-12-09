49c068b9566d1a4e3a73bc4e4d7d30471765b400
Mark Wanders <mndmwanders@gmail.com>
2021-12-09 15:32:22 +0100

    Reformatting

--- src/day09/Smoke.kt
+++ src/day09/Smoke.kt
@@ -8,29 +8,31 @@ fun main() {
     val minima = mutableListOf<Pair<Int, Int>>()
     (input.indices).forEach { y ->
         (input[y].indices).forEach { x ->
-            val neighbors = neighbors(y, x)
-            if (input[y][x] < neighbors.minOf { input[it.first][it.second] }) {
+            if (input[y][x] < neighbors(y, x).minOf { input[it.first][it.second] }) {
                 minima += y to x
             }
         }
     }
     println(minima.sumOf { 1 + Character.getNumericValue(input[it.first][it.second]) })
-    val basinSizes = mutableListOf<Int>()
-    minima.forEach { minimum ->
+    val basinSizes = minima.map { minimum ->
         val seen = mutableSetOf(minimum)
-        val neighbors = neighbors(minimum.first, minimum.second).filter { input[it.first][it.second] != '9' }.toMutableSet()
+        val neighbors = neighbors(minimum.first, minimum.second)
+            .filter { input[it.first][it.second] != '9' }
+            .toMutableSet()
         while (neighbors.iterator().hasNext()) {
             val position = neighbors.iterator().next()
             neighbors.remove(position)
             seen.add(position)
-            neighbors.addAll(neighbors(position.first, position.second).filter { input[it.first][it.second] != '9' }.filter { it !in seen })
+            neighbors.addAll(neighbors(position.first, position.second)
+                .filter { input[it.first][it.second] != '9' }
+                .filter { it !in seen })
         }
-        basinSizes += seen.size
+        seen.size
     }
-    println(basinSizes.sortedDescending().take(3).reduce { a, b -> a*b })
+    println(basinSizes.sortedDescending().take(3).reduce { a, b -> a * b })
 }
 
 private fun neighbors(y: Int, x: Int): List<Pair<Int, Int>> =
-    listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1).filter { coordinates ->
-        coordinates.first in input[y].indices && coordinates.second in input.indices
-    }.map { coordinate -> coordinate.second to coordinate.first }
+    listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1)
+        .filter { coordinates -> coordinates.first in input[y].indices && coordinates.second in input.indices }
+        .map { coordinate -> coordinate.second to coordinate.first }
