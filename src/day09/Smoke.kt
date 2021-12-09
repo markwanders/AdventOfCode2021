99ee6a521189125ca1ae964772dad73bd0b4d2ab
Mark Wanders <mndmwanders@gmail.com>
2021-12-09 10:52:23 +0100

    Solved day 9 part 2

--- src/day09/Smoke.kt
+++ src/day09/Smoke.kt
@@ -5,16 +5,32 @@ import java.io.File
 private val input = File("src/day09/input.txt").readLines().map { it.toCharArray() }
 
 fun main() {
-    var minima = 0
+    val minima = mutableListOf<Pair<Int, Int>>()
     (input.indices).forEach { y ->
         (input[y].indices).forEach { x ->
-            val neighbors = listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1).filter { coordinates ->
-                coordinates.first in input[y].indices && coordinates.second in input.indices
-            }.map { coordinate -> input[coordinate.second][coordinate.first] }
-            if (input[y][x] < neighbors.minByOrNull { Character.getNumericValue(it) }!!) {
-                minima += (1 + Character.getNumericValue(input[y][x]))
+            val neighbors = neighbors(y, x)
+            if (input[y][x] < neighbors.minOf { input[it.first][it.second] }) {
+                minima += y to x
             }
         }
     }
-    println(minima)
+    println(minima.sumOf { 1 + Character.getNumericValue(input[it.first][it.second]) })
+    val basinSizes = mutableListOf<Int>()
+    minima.forEach { minimum ->
+        val seen = mutableSetOf(minimum)
+        val neighbors = neighbors(minimum.first, minimum.second).filter { input[it.first][it.second] != '9' }.toMutableSet()
+        while (neighbors.iterator().hasNext()) {
+            val position = neighbors.iterator().next()
+            neighbors.remove(position)
+            seen.add(position)
+            neighbors.addAll(neighbors(position.first, position.second).filter { input[it.first][it.second] != '9' }.filter { it !in seen })
+        }
+        basinSizes += seen.size
+    }
+    println(basinSizes.sortedDescending().take(3).reduce { a, b -> a*b })
 }
+
+private fun neighbors(y: Int, x: Int): List<Pair<Int, Int>> =
+    listOf(x + 1 to y, x - 1 to y, x to y + 1, x to y - 1).filter { coordinates ->
+        coordinates.first in input[y].indices && coordinates.second in input.indices
+    }.map { coordinate -> coordinate.second to coordinate.first }
