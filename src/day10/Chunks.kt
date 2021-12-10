5f4f83541179822000eb7eb4b0ef413169da9f61
Mark Wanders <mndmwanders@gmail.com>
2021-12-10 11:50:18 +0100

    Solved day 10 part 2

--- src/day10/Chunks.kt
+++ src/day10/Chunks.kt
@@ -6,16 +6,21 @@ private val input = File("src/day10/input.txt").readLines()
 
 fun main() {
     val brackets = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
+    val toComplete = mutableListOf<List<Char>>()
     val errors = input.map { line ->
         val lastOpenings = mutableListOf<Char>()
         var error = false
-        line.firstOrNull { character ->
+        val errorChar = line.firstOrNull { character ->
             when (character) {
                 in brackets.keys -> lastOpenings.add(character)
                 in brackets.values -> error = character != brackets[lastOpenings.removeLast()]
             }
             error
         }
+        if (errorChar == null) {
+            toComplete.add(lastOpenings)
+        }
+        errorChar
     }.sumBy { char ->
         when (char) {
             ')' -> 3
@@ -26,4 +31,18 @@ fun main() {
         }
     }
     println(errors)
+    val completeScores = toComplete.map { line ->
+        line.reversed().map<Char, Long> {
+            when (brackets[it]) {
+                ')' -> 1
+                ']' -> 2
+                '}' -> 3
+                '>' -> 4
+                else -> 0
+            }
+        }.reduce { acc, v ->
+            (acc * 5) + v
+        }
+    }
+    println(completeScores.sorted()[completeScores.size/2])
 }
