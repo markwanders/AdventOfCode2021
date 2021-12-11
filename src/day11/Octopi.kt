eb33a77fec674e7af5cad7e7290f44461cf92604
Mark Wanders <mndmwanders@gmail.com>
2021-12-11 11:18:14 +0100

    Solved day 11 part 1

---
+++ src/day11/Octopi.kt
@@ -0,0 +1,40 @@
+package day11
+
+import java.io.File
+
+private val input = File("src/day11/input.txt").readLines().map { y -> y.split("")
+    .filter { it.isNotEmpty() }.map { it.toInt() }}
+
+fun main() {
+    val octopi = mutableMapOf<Pair<Int, Int>, Int>()
+    input.indices.forEach { y ->
+        input[y].indices.forEach { x ->
+            octopi[y to x] = input[y][x]
+        }
+    }
+    var flashCounter = 0
+    repeat(100) {
+        octopi.forEach { (k, v) -> octopi[k] = v + 1 }
+        val toFlash = octopi.entries.filter { it.value > 9 }.map { it.key }.toMutableList()
+        val flashed = mutableSetOf<Pair<Int, Int>>()
+        while (toFlash.isNotEmpty()) {
+            val next = toFlash.removeLast()
+            flashed.add(next)
+            val neighbors = neighbors(next.first, next.second)
+            neighbors.forEach {
+                octopi[it] = octopi[it]!! + 1
+            }
+            toFlash.addAll(neighbors.filter { octopi[it]!! > 9 }.filterNot { it in flashed || it in toFlash })
+
+        }
+        flashed.forEach { octopi[it] = 0 }
+        flashCounter += flashed.size
+    }
+    println(flashCounter)
+}
+
+private fun neighbors(y: Int, x: Int): List<Pair<Int, Int>> =
+    listOf(x + 1 to y + 1, x - 1 to y - 1, x + 1 to y - 1, x - 1 to y + 1, x + 1 to y, x - 1 to y, x to y + 1, x to y - 1)
+        .filter { coordinates -> coordinates.first in input[y].indices && coordinates.second in input.indices }
+        .map { coordinate -> coordinate.second to coordinate.first }
+
