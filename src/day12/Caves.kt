37522fc29ecefaf44c8f783f3d1777acb138577e
Mark Wanders <mndmwanders@gmail.com>
2021-12-12 20:25:02 +0100

    Solved day 12 part 2

--- src/day12/Caves.kt
+++ src/day12/Caves.kt
@@ -3,34 +3,45 @@ package day12
 import java.io.File
 
 private val input = File("src/day12/input.txt").readLines()
+private val caves = mutableMapOf<String, MutableList<String>>()
 
 fun main() {
-    val caves = mutableMapOf<String, MutableList<String>>()
     input.forEach {
         val (first, last) = it.split("-").run { first() to last() }
         caves[first] = (caves.getOrDefault(first, mutableListOf()) + last) as MutableList<String>
         caves[last] = (caves.getOrDefault(last, mutableListOf()) + first) as MutableList<String>
     }
+    println(caves)
+    println(traverse())
+    println(traverse(part2 = true))
+}
+
+fun traverse(part2: Boolean = false): Int {
     val start = Path("start", mutableSetOf("start"))
     val queue = mutableListOf(start)
     var paths = 0
     while (queue.isNotEmpty()) {
-        val (position, smallCaveHistory) = queue.removeFirst().run { position to smallCaveHistory }
+        val (position, smallCaveHistory, revisitedCave) =
+            queue.removeFirst().run { Triple(position, smallCaveHistory, revisitedCave) }
         if (position == "end") {
             paths++
             continue
         }
         caves.getOrDefault(position, mutableListOf()).forEach { cave ->
-            if(cave !in smallCaveHistory) {
+            if (cave !in smallCaveHistory) {
                 val newSmallCaveHistory = smallCaveHistory.toMutableSet()
-                if(cave.lowercase() == cave) {
+                if (cave.lowercase() == cave) {
                     newSmallCaveHistory += cave
                 }
-                queue.add(Path(cave, newSmallCaveHistory))
+                queue.add(Path(cave, newSmallCaveHistory, revisitedCave))
+            } else if (part2) {
+                if (cave in smallCaveHistory && revisitedCave == null && cave !in listOf("start", "end")) {
+                    queue.add(Path(cave, smallCaveHistory, cave))
+                }
             }
         }
     }
-    println(paths)
+    return paths
 }
 
-data class Path(val position: String, val smallCaveHistory: MutableSet<String>)
+data class Path(val position: String, val smallCaveHistory: MutableSet<String>, val revisitedCave: String? = null)
