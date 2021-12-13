56045e32285699d7b351fcbfe4369be5c235af87
Mark Wanders <mndmwanders@gmail.com>
2021-12-13 12:11:55 +0100

    Using fold() because we're folding

--- src/day13/Origami.kt
+++ src/day13/Origami.kt
@@ -5,36 +5,33 @@ import java.io.File
 private val input = File("src/day13/input.txt").readLines()
 
 fun main() {
-    val dots = mutableSetOf<Pair<Int, Int>>()
+    val initialDots = mutableSetOf<Pair<Int, Int>>()
     val folds = mutableListOf<Pair<Char, Int>>()
     input.forEach { line ->
         "fold along ([xy])=([0-9]*)".toRegex().find(line)?.run {
             folds.add(destructured.component1().first() to destructured.component2().toInt())
         }
         "([0-9]*),([0-9]*)".toRegex().find(line)?.run {
-            dots.add(destructured.component1().toInt() to destructured.component2().toInt())
+            initialDots.add(destructured.component1().toInt() to destructured.component2().toInt())
         }
     }
-    folds.forEachIndexed { index, (axis, coordinate) ->
-        when (axis) {
-            'y' -> dots.filter { dot -> dot.second > coordinate }.forEach { (x, y) ->
-                val folded = x to 2 * coordinate - y
-                dots.add(folded)
-                dots.remove(x to y)
+    folds.foldIndexed(initialDots) { index, dotsAcc, (axis, coordinate) ->
+        dotsAcc.map { dot ->
+            when {
+                axis == 'y' && dot.second > coordinate -> dot.first to 2 * coordinate - dot.second
+                axis == 'x' && dot.first > coordinate -> 2 * coordinate - dot.first to dot.second
+                else -> dot
             }
-            'x' -> dots.filter { dot -> dot.first > coordinate }.forEach { (x, y) ->
-                val folded = 2 * coordinate - x to y
-                dots.add(folded)
-                dots.remove(x to y)
+        }.toMutableSet().also {
+            if (index == 1) println(dotsAcc.size)
+        }
+    }.let { foldedDots ->
+        MutableList(foldedDots.maxOf { it.second } + 1) { MutableList(foldedDots.maxOf { it.first } + 1) { "  " } }.apply {
+            foldedDots.forEach { this[it.second][it.first] = "##" }
+            this.forEach { y ->
+                println(y.joinToString(""))
             }
         }
-        if (index == 0) {
-            println(dots.size)
-        }
     }
-    val image = MutableList(dots.maxOf { it.second } + 1) { MutableList(dots.maxOf { it.first } + 1) { "  " }}
-    dots.forEach { image[it.second][it.first] = "##" }
-    image.forEach { y ->
-        println(y.joinToString(""))
-    }
+
 }
