7566e25132c8d6e24bc4021ee45abb924046a611
Mark Wanders <mndmwanders@gmail.com>
2021-12-15 11:08:38 +0100

    Sped up day 15 a bit

--- src/day15/Chitons.kt
+++ src/day15/Chitons.kt
@@ -1,6 +1,7 @@
 package day15
 
 import java.io.File
+import java.util.*
 
 private val input = File("src/day15/input.txt").readLines()
 
@@ -16,15 +17,14 @@ fun main() {
 }
 
 fun shortestPath(map: MutableMap<Pair<Int, Int>, Int>, target: Pair<Int, Int>): Int {
-    val queue = mutableListOf((Node(0 to 0, 0)))
+    val queue = PriorityQueue(Comparator.comparing(Node::distance))
+    queue.add((Node(0 to 0, 0)))
+    val distances = hashMapOf<Pair<Int, Int>, Int>()
 
-    val distances = hashMapOf<Pair<Int, Int>, Int>().withDefault { 0 }
+    while (queue.isNotEmpty()) {
+        val currentNode = queue.poll()
 
-    while(queue.isNotEmpty()) {
-        queue.sortBy { it.distance }
-        val currentNode = queue.removeFirst()
-
-        if(currentNode.position in distances.keys && distances.getValue(currentNode.position) <= currentNode.distance) {
+        if (currentNode.position in distances.keys && distances.getValue(currentNode.position) <= currentNode.distance) {
             continue
         }
 
@@ -33,13 +33,9 @@ fun shortestPath(map: MutableMap<Pair<Int, Int>, Int>, target: Pair<Int, Int>): 
         if (currentNode.position == target) {
             return currentNode.distance
         }
+        val max = map.keys.maxOf { it.first } + 1
         neighbors(currentNode.position.first, currentNode.position.second, target).forEach { (ny, nx) ->
-            val max = map.keys.maxOf { it.first } + 1
-            val down =  ny / max
-            val right = nx / max
-            val projectedY = ny % max
-            val projectedX = nx % max
-            var riskValue = map.getValue(projectedY to projectedX) + down + right
+            var riskValue = map.getValue(ny % max to nx % max) + ny / max + nx / max
             while (riskValue > 9) riskValue -= 9
             queue.add(Node(ny to nx, currentNode.distance + riskValue))
         }
@@ -49,8 +45,7 @@ fun shortestPath(map: MutableMap<Pair<Int, Int>, Int>, target: Pair<Int, Int>): 
 
 private fun neighbors(y: Int, x: Int, target: Pair<Int, Int>): List<Pair<Int, Int>> =
     listOf(y + 1 to x, y - 1 to x, y to x + 1, y to x - 1)
-        .filter { coordinates -> coordinates.first >= 0 && coordinates.second >= 0 && coordinates.first <= target.first && coordinates.second <= target.second}
-        .map { coordinate -> coordinate.first to coordinate.second }
+        .filter { (y, x) -> y >= 0 && x >= 0 && y <= target.first && x <= target.second }
 
 data class Node(val position: Pair<Int, Int>, val distance: Int)
 
