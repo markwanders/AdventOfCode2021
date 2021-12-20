b48b901f8a18bc4c94572837bde2baf9742504c6
Mark Wanders <mndmwanders@gmail.com>
2021-12-20 10:49:57 +0100

    Solved day 20 part 1

--- src/day19/Beacons.kt
+++ src/day19/Beacons.kt
@@ -6,14 +6,19 @@ private val input = File("src/day19/input.txt").readLines()
 
 fun main() {
     val scanners = mutableMapOf<Int, MutableList<Triple<Int, Int, Int>>>().withDefault { mutableListOf() }
-    var scanner = 0
+    var nr = 0
     input.forEach {
-        if(' ' !in it && it.isNotBlank()) {
+        if (' ' !in it && it.isNotBlank()) {
             val parts = it.split(",").map { it.toInt() }
-            scanners[scanner] = scanners.getValue(scanner).plus(Triple(parts[0], parts[1], parts[2])).toMutableList()
-        } else if(it.isBlank()) {
-            scanner++
+            scanners[nr] = scanners.getValue(nr).plus(Triple(parts[0], parts[1], parts[2])).toMutableList()
+        } else if (it.isBlank()) {
+            nr++
         }
     }
     println(scanners)
+    scanners.entries.zipWithNext { a, b -> a.value }
+}
+
+fun distance(coordinate: Triple<Int, Int, Int>): Int {
+    return coordinate.first * coordinate.first + coordinate.second * coordinate.second + coordinate.third * coordinate.third
 }
---
+++ src/day20/TrenchMap.kt
@@ -0,0 +1,53 @@
+package day20
+
+import java.io.File
+
+private val input = File("src/day20/input.txt").readLines()
+
+fun main() {
+    val algorithm = input.first().toCharArray()
+    var image = input.drop(2)
+        .mapIndexed { y, line ->
+            line.mapIndexed { x, _ -> y to x }.associateBy({ it.first to it.second }, { line[it.second] })
+        }
+        .flatMap { it.entries }
+        .associate { it.key to it.value }
+        .toMutableMap()
+        .withDefault { '.' }
+    repeat(2) {
+        val even = (it + 1)%2 == 0
+        val newImage = mutableMapOf<Pair<Int, Int>, Char>().withDefault { if(even) '.' else '#' }
+        val yRange = image.keys.minOf { it.first } to image.keys.maxOf { it.first }
+        val xRange = image.keys.minOf { it.second } to image.keys.maxOf { it.second }
+        for (y in yRange.first - 2..yRange.second + 2) {
+            for (x in xRange.first - 2..xRange.second + 2) {
+                newImage[y to x] = algorithm[square(y, x, image)]
+            }
+        }
+        image = newImage
+        printImage(image)
+    }
+    println(image.values.sumOf { if(it == '#') 1L else 0 })
+}
+
+fun square(y: Int, x: Int, image: Map<Pair<Int, Int>, Char>): Int {
+    val string =
+        listOf(
+            image.getValue(y - 1 to x - 1), image.getValue(y - 1 to x), image.getValue(y - 1 to x + 1),
+            image.getValue(y to x - 1), image.getValue(y to x), image.getValue(y to x + 1),
+            image.getValue(y + 1 to x - 1), image.getValue(y + 1 to x), image.getValue(y + 1 to x + 1)
+        ).joinToString("")
+    return string.map { if (it == '#') '1' else '0' }.joinToString("").toInt(2)
+}
+
+fun printImage(image: Map<Pair<Int, Int>, Char>) {
+    var y = image.keys.minOf { it.first }
+    image.keys.sortedWith(compareBy({ it.first }, { it.second })).forEach {
+        if (it.first > y) {
+            println()
+            y++
+        }
+        print(image[it])
+    }
+    println()
+}
