22413b42fdd8121a85f0e6b58d8277dddc15e875
Mark Wanders <mndmwanders@gmail.com>
2021-12-22 14:19:24 +0100

    IntRanges are really slow

--- src/day22/Cubes.kt
+++ src/day22/Cubes.kt
@@ -1,26 +1,25 @@
 package day22
 
 import java.io.File
+import kotlin.math.max
+import kotlin.math.min
 
 private val input = File("src/day22/input.txt").readLines()
 
 fun main() {
-    val regex = "(on|off) x=([-\\d]*..[-\\d]*),y=([-\\d]*..[-\\d]*),z=([-\\d]*..[-\\d]*)".toRegex()
+    val regex = "(on|off) x=([-\\d]*)..([-\\d]*),y=([-\\d]*)..([-\\d]*),z=([-\\d]*)..([-\\d]*)".toRegex()
     val cubes = mutableMapOf<Triple<Int, Int, Int>, Boolean>().withDefault { false }
     val rules = input.map {
-        val (onOff, x, y, z) = regex.find(it)!!.destructured
+        val (onOff, xMin, xMax, yMin, yMax, zMin, zMax) = regex.find(it)!!.destructured
         val on = onOff == "on"
-        val xRange = x.split("..").first().toInt()..x.split("..").last().toInt()
-        val yRange = y.split("..").first().toInt()..y.split("..").last().toInt()
-        val zRange = z.split("..").first().toInt()..z.split("..").last().toInt()
-        Rule(on, xRange, yRange, zRange)
+        Rule(on, xMin.toInt(), xMax.toInt(), yMin.toInt(), yMax.toInt(), zMin.toInt(), zMax.toInt())
     }
     rules.forEach { rule ->
-        for (x in rule.x) {
+        for (x in rule.xMin..rule.xMax) {
             if (x in -50..50) {
-                for (y in rule.y) {
+                for (y in rule.yMin..rule.yMax) {
                     if (y in -50..50) {
-                        for (z in rule.z) {
+                        for (z in rule.zMin..rule.zMax) {
                             if (z in -50..50) {
                                 cubes[Triple(x, y, z)] = rule.on
                             }
@@ -33,30 +32,41 @@ fun main() {
     println(cubes.count { it.value })
     var cuboids = mutableMapOf<Cuboid, Int>()
     rules.forEach {
-        val (on, rX, rY, rZ) = it
+        val (on, rXMin, rXMax, rYMin, rYMax, rZMin, rZMax) = it
         val newCuboids = cuboids.toMutableMap().withDefault { 0 }
-        cuboids.forEach {  (cuboid, sign) ->
-            overlap(cuboid.x, rX)?.let { x ->
-                overlap(cuboid.y, rY)?.let { y ->
-                    overlap(cuboid.z, rZ)?.let { z ->
-                        val overlap = Cuboid(x, y, z)
-                        newCuboids[overlap] = newCuboids.getValue(overlap) - sign
-                    }
-                }
+        cuboids.forEach { (cuboid, sign) ->
+            val xMin = max(rXMin, cuboid.xMin)
+            val xMax = min(rXMax, cuboid.xMax)
+            val yMin = max(rYMin, cuboid.yMin)
+            val yMax = min(rYMax, cuboid.yMax)
+            val zMin = max(rZMin, cuboid.zMin)
+            val zMax = min(rZMax, cuboid.zMax)
+            if (xMin <= xMax && yMin <= yMax && zMin <= zMax) {
+                 Cuboid(xMin, xMax, yMin, yMax, zMin, zMax).apply {
+                     newCuboids[this] = newCuboids.getValue(this) - sign
+                 }
             }
         }
         if (on) {
-            newCuboids[Cuboid(rX, rY, rZ)] = newCuboids.getValue(Cuboid(rX, rY, rZ)) + 1
+            Cuboid(rXMin, rXMax, rYMin, rYMax, rZMin, rZMax).apply {
+                newCuboids[this] = newCuboids.getValue(this) + 1
+            }
         }
         cuboids = newCuboids
     }
-    val answer: Long = cuboids.entries.sumOf { (it, sign) -> sign.toLong() * (1 + it.x.last - it.x.first) * (1 + it.y.last - it.y.first) * (1 + it.z.last - it.z.first) }
+    val answer: Long =
+        cuboids.entries.sumOf { (it, sign) -> sign.toLong() * (1 + it.xMax - it.xMin) * (1 + it.yMax - it.yMin) * (1 + it.zMax - it.zMin) }
     println(answer)
 }
 
-fun overlap(a: IntRange, b: IntRange): IntRange? =
-    a.intersect(b).let { int -> if(int.isNotEmpty()) int.first()..int.last() else null }
+data class Rule(
+    val on: Boolean,
+    val xMin: Int,
+    val xMax: Int,
+    val yMin: Int,
+    val yMax: Int,
+    val zMin: Int,
+    val zMax: Int
+)
 
-
-data class Rule(val on: Boolean, val x: IntRange, val y: IntRange, val z: IntRange)
-data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange)
+data class Cuboid(val xMin: Int, val xMax: Int, val yMin: Int, val yMax: Int, val zMin: Int, val zMax: Int)
