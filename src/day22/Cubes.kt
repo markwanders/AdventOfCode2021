19d47503bc30099ec6b3ac3b9e7b84b9ebe8cde1
Mark Wanders <mndmwanders@gmail.com>
2021-12-22 14:01:19 +0100

    Solved day 22 part 2 but it takes like 15 minutes

--- src/day22/Cubes.kt
+++ src/day22/Cubes.kt
@@ -31,6 +31,32 @@ fun main() {
         }
     }
     println(cubes.count { it.value })
+    var cuboids = mutableMapOf<Cuboid, Int>()
+    rules.forEach {
+        val (on, rX, rY, rZ) = it
+        val newCuboids = cuboids.toMutableMap().withDefault { 0 }
+        cuboids.forEach {  (cuboid, sign) ->
+            overlap(cuboid.x, rX)?.let { x ->
+                overlap(cuboid.y, rY)?.let { y ->
+                    overlap(cuboid.z, rZ)?.let { z ->
+                        val overlap = Cuboid(x, y, z)
+                        newCuboids[overlap] = newCuboids.getValue(overlap) - sign
+                    }
+                }
+            }
+        }
+        if (on) {
+            newCuboids[Cuboid(rX, rY, rZ)] = newCuboids.getValue(Cuboid(rX, rY, rZ)) + 1
+        }
+        cuboids = newCuboids
+    }
+    val answer: Long = cuboids.entries.sumOf { (it, sign) -> sign.toLong() * (1 + it.x.last - it.x.first) * (1 + it.y.last - it.y.first) * (1 + it.z.last - it.z.first) }
+    println(answer)
 }
 
+fun overlap(a: IntRange, b: IntRange): IntRange? =
+    a.intersect(b).let { int -> if(int.isNotEmpty()) int.first()..int.last() else null }
+
+
 data class Rule(val on: Boolean, val x: IntRange, val y: IntRange, val z: IntRange)
+data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange)
